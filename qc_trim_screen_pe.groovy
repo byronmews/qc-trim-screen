b6212dadf173a881a17178aa511da1247e7dc299
byronmews <grahadrose@gmail.com>
2016-10-18 14:56:11 +0100

    fastqc_pre

--- qc_trim_screen_pe.groovy
+++ qc_trim_screen_pe.groovy
@@ -1,6 +1,7 @@
-// FastQC > Trimmomatic > FastQC > Fastq Screen
-
-
+// Generic Illumina QC
+//
+// Workflow: FastQC > Trimmomatic > FastQC > Fastq Screen
+// Defaults to human/univec screen config
 
 TRIMMOMATIC="/usr/local/src/trimmomatic-0.32.jar"
 SCREEN="/usr/local/etc/fastq_screen_v0.4.4_config/fastq_screen_human.conf"
@@ -15,16 +16,17 @@ fastqc_pre = {
 	transform(".fastq.gz") to ("_fastqc.zip") {
 	
 		exec "mkdir -p $output.dir; fastqc -k 8 --nogroup $input1.gz -t 12 -o $output.dir"
-		exec "fastqc -k 8 --nogroup $input2.fastq.gz -t 12 -o $output.dir"
+		exec "fastqc -k 8 --nogroup $input2.gz -t 12 -o $output.dir"
 	}
 }
 
 
 
+
 // Single sample, no parallelism
-run { fastqc_pre }
+//run { fastqc_pre }
 
 // Multiple samples where file names begin with sample
 // name and are separated by underscore from the rest of the 
 // file name
-//run { "%_*.fastq.gz" * [ fastqc_pre ] }
+run { "%_*.fastq.gz" * [ fastqc_pre ] }
