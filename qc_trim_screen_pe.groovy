06d4f8635a5891b50c8c84d4014b1b1581c51be5
byronmews <grahamdrose@gmail.com>
2016-10-21 15:47:44 +0100

    bug fix

--- qc_trim_screen_pe.groovy
+++ qc_trim_screen_pe.groovy
@@ -11,19 +11,16 @@ fastqc_pre = {
 	doc "Run fastQC on raw reads"
 	
 	// Fastqc output dir
-	output.dir = "fastqc_pre_qc"
+	output.dir = "fastqc_pre_qc"	
+
+	exec "mkdir -p $output.dir" 
+	exec "fastqc -k 8 --nogroup $input1.gz -t 12 -o $output.dir"
+	exec "fastqc -k 8 --nogroup $input2.gz -t 12 -o $output.dir"
 	
-	filter("fastqc") {
-		// How the file name is getting transformed
-		transform(".fastq.gz") to ("_fastqc.zip") {
-			exec "mkdir -p $output.dir; fastqc -k 8 --nogroup $input1.gz -t 12 -o $output.dir"
-			exec "fastqc -k 8 --nogroup $input2.gz -t 12 -o $output.dir"
-		}
-	forward input1, input2
-	}
+	forward input1.gz, input2.gz
+
 }
 
-@intermediate()
 trimmomatic_PE = {
 
 		doc "Trim reads using Trimmomatic using PE mode"
@@ -39,12 +36,9 @@ trimmomatic_PE = {
             	("$input2".replaceAll(/.*\//,"") - input_extension + '_PE.fastq'),
             	("$input2".replaceAll(/.*\//,"") - input_extension + '_SE.fastq')
 		]
-		
-		// Tidy up any previous runs within the directory
-                //exec "rm -f trimmomatic.err"
 
 		// Transform fastqc.gz to fastq
-		transform(".fastq.gz") to (".fastq") {
+		//transform(".fastq.gz") to (".fastq") {
 		produce(products) {
 		exec """
 			$TRIMMOMATIC PE -phred33 
@@ -56,29 +50,7 @@ trimmomatic_PE = {
 			2>>trimmomatic.err
 		""" 
 		}
-		// Forwarding trimmed PE fastq files only
-		forward output1, output3
-	}
 }
-
-trimmomatic_SE = {
-
-                doc "Trim reads using Trimmomatic using SE mode"
-
-                filter("trimmomatic_SE") {
-                // Transforming the fastqc gz output filename to an unzippped fastq
-                transform(".fastq.gz") to (".fastq") {
-                exec """
-                        $TRIMMOMATIC SE -phred33
-                        $input1.gz
-                        $output1.fastq ${output1.prefix}.R1_001_SE.fastq
-                        ILLUMINACLIP:/srv/data0/dbs/trimmomatic_db/contaminant_list.fasta:2:30:10
-                        LEADING:20 TRAILING:20 MINLEN:40
-                """
-                }
-        }
-}
-
 
 fastqc_post = {
 
@@ -86,16 +58,10 @@ fastqc_post = {
 
         // Fastqc output dir
         output.dir = "fastqc_post_qc"
-	filter("fastqc") {
-
-                // How the file name is getting transformed
-                transform(".fastq") to ("_fastqc.zip") {
-                        exec "mkdir -p $output.dir"
-			exec "fastqc -k 8 --nogroup $input1.fastq  -t 12 -o $output.dir"
-                        exec "fastqc -k 8 --nogroup $input2.fastq -t 12 -o $output.dir"
-		}
-		forward input1, input2
-	}
+	
+	exec "mkdir -p $output.dir"
+	exec "fastqc -k 8 --nogroup $input1.fastq  -t 12 -o $output.dir"
+        exec "fastqc -k 8 --nogroup $input3.fastq -t 12 -o $output.dir"
 }
 
 
@@ -109,7 +75,7 @@ screen = {
 		--aligner bowtie2 --threads 12 
 		--nohits 
 		--paired 
-		$input1.fastq $input2.fastq
+		$input1.fastq $input3.fastq
 	"""
 }
 
@@ -119,7 +85,7 @@ qc_summary = {
 	doc "QC stats summary file"
 	
 	exec """
-		bash qc_results_parser.pl
+		perl qc_results_parser.pl
 	"""
 
 
@@ -132,7 +98,8 @@ qc_summary = {
 //	fastqc_pre + trimmomatic_PE + fastqc_post + screen
 //}
 
-// Multiple samples where file names begin with sample name separated by underscore
+// Multiple samples where file names begin with sample name separated by regex
+// such as: 1-2_S2_L001_R1_001.fastq.gz)
 Bpipe.run {
-	"%_*.fastq.gz" * [ fastqc_pre + trimmomatic_PE + fastqc_post + screen ]
-}
+	"%_S*_L001_R*_*.fastq.gz" * [ fastqc_pre + trimmomatic_PE +  fastqc_post +  screen ] + qc_summary
+} 
